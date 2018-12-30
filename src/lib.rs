1cecff36c16c576cfbb64f8059b4594402f9d206
Andrea Lattuada <andrea@lattuada.me>
2018-12-30 05:32:53 +0100

    Concurrent implementation and basic tests
    Signed-off-by: Andrea Lattuada <andrea.lattuada@inf.ethz.ch>

---
+++ src/lib.rs
@@ -0,0 +1,275 @@
+use std::sync::Arc;
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+struct BipBuffer {
+    buf: *mut u8,
+    len: usize,
+    read: AtomicUsize,
+    write: AtomicUsize,
+    last: AtomicUsize,
+}
+
+pub struct BipBufferWriter {
+    buffer: Arc<BipBuffer>,
+    write: usize,
+}
+
+unsafe impl Send for BipBufferWriter {}
+
+pub struct BipBufferReader {
+    buffer: Arc<BipBuffer>,
+    read: usize,
+    priv_write: usize,
+    last: usize,
+}
+
+unsafe impl Send for BipBufferReader {}
+
+pub fn bip_buffer(mut buf_box: Box<[u8]>) -> (BipBufferWriter, BipBufferReader) {
+    let len = buf_box.len();
+    let buf = buf_box.as_mut_ptr();
+    Box::into_raw(buf_box);
+    let buffer = Arc::new(BipBuffer {
+        buf,
+        len,
+        read: AtomicUsize::new(0),
+        write: AtomicUsize::new(0),
+        last: AtomicUsize::new(0),
+    });
+
+    (
+        BipBufferWriter {
+            buffer: buffer.clone(),
+            write: 0,
+        },
+        BipBufferReader {
+            buffer,
+            read: 0,
+            priv_write: 0,
+            last: len,
+        },
+    )
+}
+
+#[derive(Clone, Copy)]
+struct PendingReservation {
+    start: usize,
+    len: usize,
+    wraparound: bool,
+}
+
+impl BipBufferWriter {
+    fn reserve_core(&mut self, len: usize) -> Option<PendingReservation> {
+        assert!(len > 0);
+        let read = self.buffer.read.load(Ordering::Acquire);
+        // eprintln!("try reserve: read:{:?} write:{:?}", read, self.write);
+        if self.write >= read {
+            if self.buffer.len.saturating_sub(self.write) >= len {
+                // eprintln!("reserved {} at end (from {})", len, self.write);
+                Some(PendingReservation {
+                    start: self.write,
+                    len,
+                    wraparound: false,
+                })
+            } else {
+                if read.saturating_sub(1) >= len {
+                    // eprintln!("reserved {} at beginning (from {})", len, self.write);
+                    Some(PendingReservation {
+                        start: 0,
+                        len,
+                        wraparound: true,
+                    })
+                } else {
+                    None
+                }
+            }
+        } else {
+            if (read - self.write).saturating_sub(1) >= len {
+                // eprintln!("reserved {} at end (from {})", len, self.write);
+                Some(PendingReservation {
+                    start: self.write,
+                    len,
+                    wraparound: false,
+                })
+            } else {
+                None
+            }
+        }
+    }
+
+    pub fn reserve(&mut self, len: usize) -> Option<BipBufferWriterReservation<'_>> {
+        let reserved = self.reserve_core(len);
+        if let Some(PendingReservation { start, len, wraparound }) = reserved {
+            Some(BipBufferWriterReservation { writer: self, start, len, wraparound })
+        } else {
+            None
+        }
+    }
+
+    pub fn spin_reserve(&mut self, len: usize) -> BipBufferWriterReservation<'_> {
+        assert!(len <= self.buffer.len);
+        let PendingReservation { start, len, wraparound } = loop {
+            match self.reserve_core(len) {
+                None => continue,
+                Some(r) => break r,
+            }
+        };
+        BipBufferWriterReservation { writer: self, start, len, wraparound }
+    }
+}
+
+pub struct BipBufferWriterReservation<'a> {
+    writer: &'a mut BipBufferWriter,
+    start: usize,
+    len: usize,
+    wraparound: bool,
+}
+
+impl<'a> core::ops::Deref for BipBufferWriterReservation<'a> {
+    type Target = [u8];
+
+    fn deref(&self) -> &[u8] {
+        unsafe {
+            core::slice::from_raw_parts(self.writer.buffer.buf.add(self.start), self.len)
+        }
+    }
+}
+
+impl<'a> core::ops::DerefMut for BipBufferWriterReservation<'a> {
+    fn deref_mut(&mut self) -> &mut [u8] {
+        unsafe {
+            core::slice::from_raw_parts_mut(self.writer.buffer.buf.add(self.start), self.len)
+        }
+    }
+}
+
+impl<'a> core::ops::Drop for BipBufferWriterReservation<'a> {
+    fn drop(&mut self) {
+        core::sync::atomic::fence(Ordering::Release);
+        if self.wraparound {
+            // eprintln!("writing last {}", self.writer.write);
+            self.writer.buffer.last.store(self.writer.write, Ordering::Release);
+            self.writer.write = 0;
+        }
+        self.writer.write += self.len;
+        self.writer.buffer.write.store(self.writer.write, Ordering::Release);
+        // eprintln!("drop write:{} {:?}", self.writer.write, unsafe {
+        // core::slice::from_raw_parts(self.writer.buffer.buf, self.writer.buffer.len) });
+    }
+}
+
+impl<'a> BipBufferWriterReservation<'a> {
+    pub fn send(self) {
+        // drop
+    }
+}
+
+impl BipBufferReader {
+    pub fn valid(&mut self) -> &mut [u8] {
+        self.priv_write = self.buffer.write.load(Ordering::Acquire);
+        // eprintln!("valid (write):{:?} read:{:?} last:{:?}", self.priv_write, self.read, self.last);
+        if self.priv_write >= self.read {
+            core::sync::atomic::fence(Ordering::Acquire);
+            unsafe {
+                // eprintln!("slice (1) {} {}", self.read, self.priv_write);
+                core::slice::from_raw_parts_mut(self.buffer.buf.add(self.read), self.priv_write - self.read)
+            }
+        } else {
+            self.last = self.buffer.last.load(Ordering::Acquire);
+            if self.read == self.last {
+                // eprintln!("moving last (v)");
+                self.read = 0;
+                self.last = self.buffer.len;
+                self.buffer.last.store(self.last, Ordering::Release);
+                return self.valid();
+            }
+            core::sync::atomic::fence(Ordering::Acquire);
+            unsafe {
+                // eprintln!("slice (2) {} {}", self.last, self.read);
+                core::slice::from_raw_parts_mut(self.buffer.buf.add(self.read), self.last - self.read)
+            }
+        }
+    }
+
+    pub fn consume(&mut self, len: usize) -> bool {
+        // eprintln!("consume (write):{:?} read:{:?}", self.priv_write, self.read);
+        if self.priv_write >= self.read {
+            if len <= self.priv_write - self.read {
+                self.read += len;
+            } else {
+                return false;
+            }
+        } else {
+            let remaining = self.last - self.read;
+            if len == remaining {
+                // eprintln!("moving last (c)");
+                self.read = 0;
+                self.last = self.buffer.len;
+                self.buffer.last.store(self.last, Ordering::Release);
+            } else if len <= remaining {
+                self.read += len;
+            } else {
+                return false;
+            }
+        }
+        self.buffer.read.store(self.read, Ordering::Release);
+        // eprintln!("consumed (write):{:?} read:{:?} last:{:?}", self.priv_write, self.read, self.last);
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::bip_buffer;
+
+    #[test]
+    fn basic() {
+        for i in 0..128 {
+            let (mut writer, mut reader) = bip_buffer(vec![0u8; 16].into_boxed_slice());
+            let sender = ::std::thread::spawn(move || {
+                writer.reserve(8).as_mut().expect("reserve").copy_from_slice(&[10, 11, 12, 13, 14, 15, 16, i]);
+            });
+            let receiver = ::std::thread::spawn(move || {
+                while reader.valid().len() < 8 {}
+                assert_eq!(reader.valid(), &[10, 11, 12, 13, 14, 15, 16, i]);
+            });
+            sender.join();
+            receiver.join();
+        }
+    }
+
+    #[test]
+    fn static_prime_length() {
+        const msg_length: u8 = 17; // intentionally prime
+        let (mut writer, mut reader) = bip_buffer(vec![128u8; 64].into_boxed_slice());
+        let sender = ::std::thread::spawn(move || {
+            let mut msg = [0u8; msg_length as usize];
+            for _ in 0..1024 {
+                for i in 0..128u8 {
+                    &mut msg[..].copy_from_slice(&[i; msg_length as usize][..]);
+                    msg[i as usize % (msg_length as usize)] = 0;
+                    // eprintln!(">>>>>>> {} {:?}", i, msg);
+                    writer.spin_reserve(msg_length as usize).copy_from_slice(&msg[..]);
+                }
+            }
+        });
+        let receiver = ::std::thread::spawn(move || {
+            let mut msg = [0u8; msg_length as usize];
+            for _ in 0..1024 {
+                for i in 0..128u8 {
+                    // eprintln!("<<<<<<< {}", i);
+                    &mut msg[..].copy_from_slice(&[i; msg_length as usize][..]);
+                    msg[i as usize % (msg_length as usize)] = 0;
+                    while reader.valid().len() < (msg_length as usize) {}
+                    // eprintln!("<<<<<<< checking {} {:?}", i, msg);
+                    assert_eq!(&reader.valid()[..msg_length as usize], &msg[..]);
+                    assert!(reader.consume(msg_length as usize));
+                    // eprintln!("<<<<<<< ok {}", i);
+                }
+            }
+        });
+        sender.join();
+        receiver.join();
+    }
+
+}
