59ced97962298aa81d890c620be8a108af9896d4
Andrea Lattuada <andrea@lattuada.me>
2019-01-29 17:22:19 +0100

    More detailed perf
    Signed-off-by: Andrea Lattuada <andrea.lattuada@inf.ethz.ch>

--- examples/perf.rs
+++ examples/perf.rs
@@ -1,6 +1,40 @@
+#![feature(asm)]
+
 use spsc_bip_buffer::bip_buffer;
 
-const REPETITIONS: usize = 1<<18;
+const REPETITIONS: usize = 1<<20;
+
+// ==================================
+
+// This section is from https://github.com/valarauca/amd64_timer
+//
+// Copyright (c) 2016 Willliam Cody Laeder
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+#[no_mangle]
+#[inline(never)]
+#[cfg(target_arch = "x86_64")]
+pub fn tsc_ticks() -> u64 {
+    let mask = 0xFFFFFFFFu64;
+    let high: u64;
+    let low: u64;
+    unsafe {
+        asm!("lfence;rdtsc"
+            : "={edx}"(high), "={eax}"(low)
+            :
+            : "rdx", "rax"
+            : "volatile"
+        );
+    }
+    ((high)<<32) | (mask&low)
+}
+
+// ==================================
 
 fn main() {
     let mut args = ::std::env::args();
@@ -19,6 +53,7 @@ fn main() {
     let (mut writer, mut reader) = bip_buffer(vec![0u8; 16<<10].into_boxed_slice());
     let sender = ::std::thread::spawn(move || {
         core_affinity::set_for_current(sender_core_id);
+        let mut sender_hist = streaming_harness_hdrhist::HDRHist::new();
 
         let mut msg = [0u8; LENGTH];
         for _ in 0..REPETITIONS {
@@ -28,11 +63,15 @@ fn main() {
                 for i in 1..length {
                     msg[i as usize] = round;
                 }
+                let start = tsc_ticks();
                 writer.spin_reserve(length as usize).copy_from_slice(&msg[..length as usize]);
+                let stop = tsc_ticks();
+                sender_hist.add_value(stop - start);
             }
         }
 
         eprintln!("sender done");
+        sender_hist
     });
     let receiver = ::std::thread::spawn(move || {
         core_affinity::set_for_current(receiver_core_id);
@@ -53,7 +92,7 @@ fn main() {
                     }
                     assert_eq!(&recv_msg[..LENGTH], &msg[..]);
                 }
-                assert!(reader.consume(LENGTH as usize));
+                assert!(reader.consume(LENGTH));
                 msgs_received += 1;
             }
         }
@@ -67,6 +106,7 @@ fn main() {
         let msgs_per_sec = msgs_received as f64 / ((nanos as f64) / 1_000_000_000f64);
         println!("{}\t{}\t{}\t{}\t{}", nanos, bytes_received, msgs_received, bytes_per_sec, msgs_per_sec);
     });
-    sender.join().unwrap();
+    let hist = sender.join().unwrap();
+    eprintln!("{}", hist.summary_string());
     receiver.join().unwrap();
 }
